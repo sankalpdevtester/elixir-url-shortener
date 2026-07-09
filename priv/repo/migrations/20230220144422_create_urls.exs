ca1ccc85cecf3d3aae0773d66c6f76c30154eb73
sankalpdevtester <ohofufic0@gmail.com>
2026-07-09 21:43:58 +0530

    feat: add migration for urls table

---
+++ priv/repo/migrations/20230220144422_create_urls.exs
@@ -0,0 +1,14 @@
+defmodule ElixirUrlShortener.Repo.Migrations.CreateUrls do
+  use Ecto.Migration
+
+  def change do
+    create table(:urls) do
+      add :original_url, :string
+      add :short_url, :string
+
+      timestamps()
+    end
+
+    create unique_index(:urls, [:short_url])
+  end
+end
